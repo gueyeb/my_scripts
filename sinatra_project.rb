66501bddef869b65a9c96a5f4363fa92a8f0b6f9
Babacar Gueye <babacar.gueye@epitech.eu>
2014-07-09 15:34:12 +0200

    sinatra ruby project generator

---
+++ sinatra_project.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+## 'sinatra_project.rb' ##
+
+def main
+  ## main method of program. run first
+
+end
+
+if __FILE__ == $0
+  # Do something.. run tests, call a method, etc. We're direct.
+
+  main
+end
+
