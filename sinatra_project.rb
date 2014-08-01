ec32e041c323b768440357bd3dc5aee9292d2f25
Babacar Gueye <babacar.gueye@epitech.eu>
2014-08-01 16:21:29 +0200

    add notes on scripts

--- sinatra_project.rb
+++ sinatra_project.rb
@@ -2,14 +2,9 @@
 
 ## 'sinatra_project.rb' ##
 
+#
+
 def main
-  ## main method of program. run first
-
-end
-
-if __FILE__ == $0
-  # Do something.. run tests, call a method, etc. We're direct.
-
-  main
+  puts "create a gem and basta !!!"
 end
 
